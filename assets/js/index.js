97d93860f82c9ef9949426c8a1a1e23a4da54905
Anna Novak <anna.c.novak@gmail.com>
2021-08-07 21:22:23 +0200

    Create js files

---
+++ assets/js/index.js
@@ -0,0 +1,3 @@
+/* platsen ligga på latitud: 57.3081 och longitud: 18.1489 */
+/* https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.1489/lat/57.3081/data.json */
+/* All times are in UTC (Coordinated Universal Time).  */
