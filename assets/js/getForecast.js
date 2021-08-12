ff65ff24e146f0919d796e57e38e515f72c574ad
Anna Novak <anna.c.novak@gmail.com>
2021-08-12 20:45:41 +0200

    Get data from smhi

---
+++ assets/js/getForecast.js
@@ -0,0 +1,4 @@
+export async function getForecast() {
+  const response = await fetch('https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.1489/lat/57.3081/data.json')
+  return await response.json()
+}
