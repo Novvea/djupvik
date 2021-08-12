d88c6f73780eaba1f42ff1123a274e5b919f0620
Anna Novak <anna.c.novak@gmail.com>
2021-08-12 20:46:29 +0200

    Get weatherdata to index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,11 @@
-/* platsen ligga på latitud: 57.3081 och longitud: 18.1489 */
-/* https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.1489/lat/57.3081/data.json */
-/* All times are in UTC (Coordinated Universal Time).  */
+import {getForecast} from './getForecast.js'
+
+async function showForecast() {
+  let weatherData = await getForecast()
+  console.log(weatherData)
+}
+
+showForecast() 
+
+
+
